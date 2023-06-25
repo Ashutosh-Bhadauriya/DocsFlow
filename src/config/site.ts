aa842d4247b63206af99d8d2c99003a3bdde65bf
Ashutosh-Bhadauriya <ashutoshb455@gmail.com>
2023-06-25 22:15:10 +0530

    Initial commit

---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+    name: "DocsFlow",
+    url: "https://docsflow.vercel.app",
+    ogImage: "https://docsflow.vercel.app/opengraph-image.png",
+    description:
+      "Open source alternative to Docsend",
+    links: {
+      twitter: "https://twitter.com/b__ashutosh",
+      github: "https://github.com/Ashutosh-Bhadauriya",
+    },
+  }
+  
+  export type SiteConfig = typeof siteConfig
+  
